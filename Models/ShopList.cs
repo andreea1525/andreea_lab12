5f639d0b6aaa3bab9b16d14fa4f3b8e1f2a07817
andreea1525 <97646321+andreea1525@users.noreply.github.com>
2022-01-13 21:18:10 +0200

    Add files via upload

--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace lab12.Models
+namespace WebAPI.Models
 {
     public class ShopList
     {
